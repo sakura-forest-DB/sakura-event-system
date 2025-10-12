29678a854bc3dbf28749cb79ffb4a8b24ea68aa6
sakura-forest-DB <sfcmoroi@gmail.com>
2025-10-12 15:31:34 +0900

    Update and rename lib to src/lib/prisma.js

---
+++ src/lib/prisma.js
@@ -0,0 +1,11 @@
+// src/lib/prisma.js (ESM)
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = globalThis;
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
